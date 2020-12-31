b6141b5a36537a203525de9450f37bb5981c65b1
Joseph Freeman <freemanjj1992@gmail.com>
2020-12-31 13:02:42 -0500

    Updating phrase reacting to and response phrase as well as adding Discord token for my bot.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const Eris = require('eris');
  
-const bot = new Eris(process.env.DISCORD_BOT_TOKEN);   // Replace DISCORD_BOT_TOKEN in .env with your bot accounts token
+const bot = new Eris(process.env.NzkxMDU4NDQ3ODk4MjQ3MjE4.X-Jolw.8pOd7bMr0OuCX8yU3vy9V834x5Q);   // Replace DISCORD_BOT_TOKEN in .env with your bot accounts token
  
 bot.on('ready', () => {                                // When the bot is ready
     console.log('Ready!');                             // Log "Ready!"
 });
  
 bot.on('messageCreate', (msg) => {                     // When a message is created
-    if(msg.content === '1337') {                       // If the message content is "1337"
-        bot.createMessage(msg.channel.id, 'damn it');  // Send a message in the same channel with "damn it"
+    if(msg.content === 'Do the Dew') {                       // If the message content is "Do the Dew"
+        bot.createMessage(msg.channel.id, 'Thank you fellow Dew Mate, and remember to reach for an ice cold Mountain Dew when gaslighting your friends!');  // Send a message in the same channel with false Mountain Dew ad.
     }
 });
  
